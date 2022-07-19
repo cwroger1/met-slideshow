58552f4a42c8335700553f5835f8f9b9e9e5bff0
cwroger1 <cwroger1@asu.edu>
2022-07-19 14:39:46 -0600

    Implement loading images using MetMuseumAPI, need to replace not append

--- met_art_site/smartframe/views.py
+++ met_art_site/smartframe/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+import requests
 
 # Create your views here.
 def index(request):
-    return render(request, "smartframe/index.html")
+    response = requests.get('https://collectionapi.metmuseum.org/public/collection/v1/objects').json()
+    return render(request, "smartframe/index.html", {
+        'response': response
+    })
