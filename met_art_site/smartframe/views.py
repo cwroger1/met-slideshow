177bead9973c58ca6c8c782c2daf808c676536ff
cwroger1 <cwroger1@asu.edu>
2022-07-21 12:05:15 -0600

    Raise a Django error if API response status code is an error

--- met_art_site/smartframe/views.py
+++ met_art_site/smartframe/views.py
@@ -3,7 +3,9 @@ import requests
 
 # Create your views here.
 def index(request):
-    response = requests.get('https://collectionapi.metmuseum.org/public/collection/v1/search?hasImages=true&q=flowers').json()
+    response = requests.get('https://collectionapi.metmuseum.org/public/collection/v1/search?hasImages=true&q=flowers')
+    if response.status_code > 299:
+        response.raise_for_status()
     return render(request, "smartframe/index.html", {
-        'response': response,
+        'response': response.json()
     })
