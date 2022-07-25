e74352c6cb2dc59f547741c6f3eed05067e87e3a
cwroger1 <cwroger1@asu.edu>
2022-07-25 13:29:04 -0600

    Create met_url variable for maintainability

--- met_art_site/smartframe/views.py
+++ met_art_site/smartframe/views.py
@@ -3,7 +3,8 @@ import requests
 
 # Create your views here.
 def index(request):
-    response = requests.get('https://collectionapi.metmuseum.org/public/collection/v1/search?hasImages=true&q=flowers')
+    met_url = "https://collectionapi.metmuseum.org/public/collection/v1/search?hasImages=true&q=flowers"
+    response = requests.get(met_url)
     if response.status_code > 299:
         response.raise_for_status()
     return render(request, "smartframe/index.html", {
